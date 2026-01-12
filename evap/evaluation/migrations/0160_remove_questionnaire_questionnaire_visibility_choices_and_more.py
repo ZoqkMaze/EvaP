84422c8cf38f18738416cb9b91597ff8250236b7
ZoqkMaze <155395129+ZoqkMaze@users.noreply.github.com>
2026-01-12 19:59:53 +0100

    fix migration and comment

--- evap/evaluation/migrations/0160_remove_questionnaire_questionnaire_visibility_choices_and_more.py
+++ evap/evaluation/migrations/0160_remove_questionnaire_questionnaire_visibility_choices_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("evaluation", "0157_coursetype_skip_on_automated_import"),
+        ("evaluation", "0159_semester_cms_name_semester_default_course_end_date"),
     ]
 
     operations = [
